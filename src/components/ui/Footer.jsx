9a02e14a7529a7b5d1448be5cee7f7bb5718bbcb
AmineCoder30 <99543252+AmineCoder30@users.noreply.github.com>
2024-12-04 12:54:03 +0100

    Add files via upload

---
+++ src/components/ui/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="w-full py-3 text-center text-white">
+      <p>
+        Made by
+        <a
+          href="https://github
+        .com/AmineCoder30"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-orange-500"
+        >
+          Mohamed Amine😁
+        </a>
+        ©Copyright 2025
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
